1a8f104d0c7b102cbc9b49020dd2e5d0f27e8b72
Abril Aguilar Lopez <abril124@ucla.edu>
2023-08-28 20:41:01 -0700

    leds show same brightness and flash at beginning of trials

--- project_4al.ino
+++ project_4al.ino
@@ -5,7 +5,11 @@
 // An analog signal is one that can take on any number of values,
 // unlike a digital signal which has only two values: HIGH and LOW
 
-int ledBrightness = 125; // will use same for all LEDS
+// each LED needs own brightness level
+#define redBrightness 190
+#define greenBrightness 10
+#define blueBrightness 1500
+
 int redState = LOW;
 int greenState = LOW;
 int blueState = LOW;
@@ -20,16 +24,38 @@ int greenButtonState = LOW;
 #define blueButtonPin 9
 int blueButtonState = LOW;
 
+/* add later
+// Time variables
+long randDelay;
+long newT;
+long oldT;
+long deltaT;
+*/
+
 void setup()
 {
     // initialize analog pins as an output
     pinMode(redPin, OUTPUT);
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
-    // initialize button pin as input
+
+    // initialize button pins as input
     pinMode(redButtonPin, INPUT);
     pinMode(greenButtonPin, INPUT);
     pinMode(blueButtonPin, INPUT);
+
+    Serial.println("Prepare. Reaction time experiments will begin shortly!");
+    for (int i = 0; i <= 3; i++)
+    {
+        analogWrite(redPin, redBrightness);
+        analogWrite(greenPin, greenBrightness);
+        analogWrite(bluePin, blueBrightness);
+        delay(250);
+        analogWrite(redPin, 0);
+        analogWrite(greenPin, 0);
+        analogWrite(bluePin, 0);
+        delay(250);
+    }
 }
 
 void loop()
